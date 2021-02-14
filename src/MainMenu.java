06755c7a4afc15483141e30eb21276fec60590e3
ManjotKhangura1 <59262828+ManjotKhangura1@users.noreply.github.com>
2021-02-13 19:33:52 -0700

    ...

---
+++ src/MainMenu.java
@@ -0,0 +1,512 @@
+//package src;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.Scanner;
+
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+/**
+ * Creates main menu when game loads up
+ */
+
+
+public class MainMenu extends Handler{
+
+    /**
+     * State of the menu
+     */
+    private static MenuState state = MenuState.mainmenu;
+    /**
+     * List of buttons
+     */
+    private static LinkedList<Button> buttonList;
+    /**
+     * handler object
+     */
+    private Handler handler;
+    /**
+     * stage used for load function
+     */
+    private Stage mainStage;
+    /**
+     * Boolean that tells the display when and when not to update
+     */
+    private boolean timeToUpdate = true;
+
+
+    /**
+     * Constructor for the Main Menu
+     * @param h - Handler that executes the program
+     * @param mainStage - Window for file viewer
+     */
+    public MainMenu(Handler h, Stage mainStage) {
+        buttonList = new LinkedList<Button>();
+        this.handler = h;
+        this.mainStage = mainStage;
+    }
+
+    /**
+     * Method that adds buttons to display list
+     */
+    public void render(Display d) {
+        for (Button b:buttonList) {
+            b.render(d);
+        }
+    }
+
+    /**
+     * Method that controls click locations
+     * @param x - Coordinate on the Horizontal plane
+     * @param y - Coordinate on the Vertical Plane
+     */
+    public void recieveClick(double x, double y) {
+        for (Button b:buttonList) {
+            if (b.coordinatesAreInside((int)x, (int)y)) {
+                b.menuTriggerClick();
+            }
+        }
+    }
+
+    /**
+     * method that sets the menu state
+     * @param state - Menu State you want to change to.
+     */
+    public void setState(MenuState state) {
+        MainMenu.state = state;
+        this.timeToUpdate = true;
+    }
+
+    /**
+     * Method that takes name of button and executes action
+     * @param text - Name of the button.
+     */
+    public void menuClickHandler(String text) {
+        if(text == "Play game") {
+            setState(MenuState.playmenu);
+        }
+        else if (text == "Load game") {
+            setState(MenuState.loadmenu);
+        }
+        else if (text == "Options") {
+            setState(MenuState.optionsmenu);
+        }
+        else if (text == "Quit") {
+            System.exit(1);
+        }
+        else if (text == "Back") {
+            setState(MenuState.mainmenu);
+        }
+        else if (text == "Pick File") {
+            load();
+        }
+        else if (text == "^") {
+            Main.arenaHeight = Main.arenaHeight + 100;
+            Main.arenaWidth = Main.arenaWidth + 100;
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "v") {
+            Main.arenaHeight = Main.arenaHeight - 100;
+            Main.arenaWidth = Main.arenaWidth - 100;
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "Defaults") {
+            Main.arenaHeight = 1500;
+            Main.arenaWidth = 2000;
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "^ ") {
+            //handler.setNumOfTraps(handler.getNumOfTraps() + 5);
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "v ") {
+            //handler.setNumOfTraps(handler.getNumOfTraps() - 5);
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "Defaults ") {
+            //handler.setNumOfTraps(5);
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "^  ") {
+            //handler.setNumOfObstacles(handler.getNumOfObstacles() + 5);
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "v  ") {
+            //handler.setNumOfObstacles(handler.getNumOfObstacles() - 5);
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        else if (text == "Defaults  ") {
+            //handler.setNumOfObstacles(10);
+            //handler.removeByID(ID.Obstacle);
+            //handler.removeByID(ID.Trap);
+            handler.setup();
+        }
+        this.timeToUpdate = true;
+    }
+
+    /**
+     * method that Updates and controls which buttons are displayed
+     */
+    public void update() {
+        if (!timeToUpdate)
+        {
+            return;
+        }
+        this.timeToUpdate = false;
+        reset();
+        if (state == MenuState.mainmenu) {
+            renderMainMenu();
+        }
+        else if (state == MenuState.loadmenu) {
+            renderLoadMenu();
+        }
+        else if (state == MenuState.optionsmenu) {
+            renderOptionsMenu();
+        }
+        else if (state == MenuState.playmenu) {
+            renderPlayMenu();
+        }
+    }
+
+    /**
+     * Method that clears the button list
+     */
+    public void reset() {
+        while (buttonList.size() > 0) {
+            buttonList.remove();
+        }
+    }
+    /**
+     * method that adds a button to the list to be displayed
+     * @param b - Newly created button.
+     */
+    public void addButton(Button b) {
+        b.setMenu(this);
+        buttonList.add(b);
+    }
+    /**
+     * method that changes the game state to Play, it changes from main to play.
+     */
+    public void renderPlayMenu() {
+        handler.setGameStatePlay();
+
+    }
+    /**
+     * Method that picks a save file and adds it all the objects to the
+     * handler.
+     */
+    public void load() {
+
+        String stringOfObjects = "";
+        File f = filePicker();
+        try {
+            Scanner myReader = new Scanner(f);
+            while (myReader.hasNextLine()) {
+                String data = myReader.nextLine();
+                stringOfObjects += data;
+            }
+            myReader.close();
+        } catch (FileNotFoundException e) {
+            System.err.println("An Error occored while loading");
+            e.printStackTrace();
+        }
+
+        //System.out.println(stringOfObjects);
+
+        int iIndex = 0;
+        int fIndex = stringOfObjects.indexOf(',');
+
+        handler.removeAll();
+
+        //System.out.println(stringOfObjects.subSequence(iIndex, fIndex));
+        Main.arenaHeight = Integer.parseInt(stringOfObjects.subSequence(iIndex, fIndex).toString());
+        iIndex = fIndex+1;
+        //System.out.println(iIndex);
+        //System.out.println(fIndex);
+        fIndex = stringOfObjects.indexOf(',',fIndex + 1);
+
+        Main.arenaWidth = Integer.parseInt(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+        //System.out.println(iIndex);
+        //System.out.println(fIndex);
+
+        iIndex = fIndex+1;
+        fIndex = stringOfObjects.indexOf(',',iIndex);
+
+        //System.out.println(stringOfObjects.subSequence(iIndex, fIndex));
+
+        while (fIndex < stringOfObjects.length() -1) {
+
+            //System.out.println(stringOfObjects.subSequence(iIndex, fIndex));
+            String object = stringOfObjects.substring(iIndex, fIndex).toString();
+            //System.out.println(object);
+            if (object.charAt(0)=='P') {
+                //System.out.println("Im a Player!!");
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float x = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float y = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float velX = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float velY = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                //float skills = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());(since skills aren't implemented yet
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                int health = Integer.parseInt(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                handler.addObject(new Player((int)x,(int)y,ID.Player,(int)velX,(int)velY, handler,health));
+            }
+            else if (object.charAt(0)=='E') {
+                //System.out.println("Im a Enemy!!");
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float x = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float y = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float velX = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float velY = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                //float velY = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());(since skills arnt implemented yet
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                int health = Integer.parseInt(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                handler.addObject(new Enemy((int)x,(int)y,ID.Enemy,(int)velX,(int)velY, handler,health));
+            }
+            else if (object.charAt(0)=='A') {
+                //System.out.println("Im a ally!!");
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float x = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float y = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float velX = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float velY = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                //float Skills = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());(since skills arnt implemented yet)
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                int health = Integer.parseInt((String) stringOfObjects.subSequence(iIndex, fIndex));
+
+                handler.addObject(new Ally((int)x,(int)y,ID.Ally,(int)velX,(int)velY, handler,health));
+            }
+            else if (object.charAt(0)=='O') {
+                //System.out.println("Im an obstacle!!");
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                int radius = Integer.parseInt(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float x = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float y = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                handler.addObject(new Obstacle((int)x,(int)y,ID.Obstacle,handler,radius));
+            }
+            else if (object.charAt(0)=='T') {
+                //System.out.println("Im a trap!!");
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                int radius = Integer.parseInt(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float x = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float y = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                handler.addObject(new Trap((int)x,(int)y,ID.Trap,handler,radius));
+            }
+            else if (object.charAt(0)=='B') {
+                //System.out.println("Im a bullet!!");
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float x = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+                float y = Float.parseFloat(stringOfObjects.subSequence(iIndex, fIndex).toString());
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+
+                handler.addObject(new Bullet((int)x,(int)y,ID.Bullet,handler));
+            }
+            if(fIndex < stringOfObjects.length() -1) {
+                iIndex = fIndex+1;
+                fIndex = stringOfObjects.indexOf(',',iIndex);
+            }
+            //System.out.println("lenght is " + stringOfObjects.length());
+            //System.out.println("index is " + fIndex);
+
+        }
+
+        handler.setGameStatePlay();
+    }
+    /**
+     * method that launches a window to select the save file
+     * @return - File of the Save.
+     */
+    public File filePicker() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Open Resource File");
+        fileChooser.getExtensionFilters().addAll(
+                new ExtensionFilter("Text Files", "*.txt"),
+                new ExtensionFilter("All Files", "*.*"));
+        File selectedFile = fileChooser.showOpenDialog(mainStage);
+        return selectedFile;
+    }
+    /**
+     * method that creates the buttons for the options menu
+     */
+    public void renderOptionsMenu() {
+        Color cc = Color.color(0, 0, 0.5);
+
+        addButton(new Button(50,30,400,100,cc,"Change Arena Size"));
+
+        addButton(new Button(50,30,100,100,cc, Main.arenaHeight + "," + Main.arenaWidth));
+
+        addButton(new Button(500,30,100,100,cc,"^"));
+
+        addButton(new Button(610,30,100,100,cc,"v"));
+
+        addButton(new Button(750,30,150,100,cc,"Defaults"));
+
+        addButton(new Button(50,210,400,100,cc,"Amount of Traps"));
+
+        //addButton(new Button(50,210,100,100,cc, handler.getNumOfTraps() + ""));
+
+        addButton(new Button(500,210,100,100,cc,"^ "));
+
+        addButton(new Button(610,210,100,100,cc,"v "));
+
+        addButton(new Button(750,210,150,100,cc,"Defaults "));
+
+        //addButton(new Button(50,390,400,100,cc,"Amount of Obsticles"));
+
+        //addButton(new Button(50,390,100,100,cc, handler.getNumOfObstacles() + ""));
+
+        addButton(new Button(500,390,100,100,cc,"^  "));
+
+        addButton(new Button(610,390,100,100,cc,"v  "));
+
+        addButton(new Button(750,390,150,100,cc,"Defaults  "));
+
+        addButton(new Button(280,570,400,100,cc,"Back"));
+
+    }
+    /**
+     * method that creates the buttons for the load menu
+     */
+    public void renderLoadMenu() {
+        Color c = Color.color(0, 0.5, 0);
+
+        addButton(new Button(280,30,400,100,c,"Pick File"));
+
+        addButton(new Button(280,570,400,100,c,"Back"));
+    }
+    /**
+     * method that creates the buttons for the Main menu.
+     */
+    public void renderMainMenu() {
+        Color c = Color.color(0, 0.5, 0.5);
+
+        addButton(new Button(280,30,400,100,c,"Play game"));
+
+        addButton(new Button(280,210,400,100,c,"Load game"));
+
+        addButton(new Button(280,390,400,100,c,"Options"));
+
+        addButton(new Button(280,570,400,100,c,"Quit"));
+
+
+    }
+
+}
---
+++ src/PauseMenu.java
@@ -0,0 +1,154 @@
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.LinkedList;
+
+import javafx.scene.paint.Color;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+
+public class PauseMenu extends Handler {
+
+    /**
+     * State of the PauseMenu
+     */
+    private static PauseState state = PauseState.mainPause;
+    /**
+     * List of buttons used
+     */
+    private static LinkedList<Button> buttonList;
+    /**
+     * Object of the handler
+     */
+    private Handler handler;
+    /**
+     * Stage used for the Save feature
+     */
+    private Stage mainStage;
+
+    /**
+     * Constructor for the pause menu.
+     * @param h - Handler for the PauseMenu
+     */
+    public PauseMenu(Handler h) {
+        buttonList = new LinkedList<Button>();
+        this.handler = h;
+    }
+
+    /**
+     * Method that renders the buttons in the Pause Menu.
+     */
+    public void render(Display d) {
+        for (Button b:buttonList) {
+            b.render(d);
+        }
+    }
+
+    /**
+     * method that handles if the coordinates of the click are in one of the buttons
+     * @param x - coordinate of the click on the Horizontal plane
+     * @param y - coordinate of the click on the vertical plane
+     */
+    public void recieveClick(double x, double y) {
+        for (Button b:buttonList) {
+            if (b.coordinatesAreInside((int)x, (int)y)) {
+                b.pauseTriggerClick();
+            }
+        }
+    }
+    /**
+     * method that uses the name of the button to dictate what happeneds when the button is clicked
+     * @param text - name of the button
+     */
+    public void menuClickHandler(String text) {
+
+        if(text == "Resume") {
+            handler.setGameStatePlay();
+        }
+        else if (text == "Save") {
+            state = PauseState.save;
+        }
+        else if (text == "Quit") {
+            System.exit(1);
+        }
+        else if (text == "Main Menu") {
+            setGameStateMainMenu();
+        }
+    }
+
+    /**
+     * This method controls what is shown on the screen, by interpreting the PauseMenu state.
+     */
+    public void update() {
+        reset();
+        if (state == PauseState.mainPause) {
+            renderPauseMenu();
+        }
+        else if (state == PauseState.save) {
+            renderSaveMenu();
+        }
+    }
+    /**
+     * Method that removes all the buttons from the button list.
+     */
+    public void reset() {
+        while (buttonList.size() > 0) {
+            buttonList.remove();
+        }
+    }
+    /**
+     * method that adds a new button to be rendered.
+     * @param b - Button to be added to the list
+     */
+    public void addButton(Button b) {
+        b.setPauseMenu(this);
+        buttonList.add(b);
+    }
+    /**
+     * method that takes a created file and saves the list of the handler.
+     */
+    private void renderSaveMenu() {
+        String objects = handler.toString();
+
+        try {
+            FileWriter save = new FileWriter(pathSelecter());
+            save.append(objects);
+            save.close();
+
+        } catch (IOException e) {
+            System.out.println("Cannot Save at this time");
+            e.printStackTrace();
+        }
+
+        state = PauseState.mainPause;
+    }
+    /**
+     * Prompts the user for a file location and a name.
+     * @return - Returns a newly created file, for saving
+     */
+    public File pathSelecter() {
+        Calendar rightNow = Calendar.getInstance();
+        FileChooser fileSelecter = new FileChooser();
+        fileSelecter.setTitle("Select File Path");
+        fileSelecter.setInitialFileName("Save" + rightNow.getTimeInMillis() + ".txt");
+        File selectedFile = fileSelecter.showSaveDialog(mainStage);
+        return selectedFile;
+    }
+    /**
+     * Method that creates the buttons for the Pause Menu.
+     */
+    public void renderPauseMenu() {
+        Color c = Color.BLUE;
+        addButton(new Button(280,30,400,100,c,"Resume"));
+
+        addButton(new Button(280,210,400,100,c,"Save"));
+
+        addButton(new Button(280,390,400,100,c,"Quit"));
+
+        //addButton(new OurButton(280,570,400,100,c,"Main Menu"));
+
+    }
+}
