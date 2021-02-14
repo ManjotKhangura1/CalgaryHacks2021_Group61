e5162d49823a04c04efea27ec3f28e486ea6f980
ManjotKhangura1 <59262828+ManjotKhangura1@users.noreply.github.com>
2021-02-14 08:24:36 -0700

    ...

--- src/Player.java
+++ src/Player.java
@@ -3,6 +3,13 @@ import javafx.scene.paint.Color;
 
 public class Player extends GameObject{
 
+    int[] playerSpriteBack = {1, 2};
+    int[] playerSpriteBackIdle = {1, 2, 3, 4, 5, 6, 7, 8};
+    int[] playerSpriteFront = {1, 2};
+    int[] playerSpriteFrontIdle = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30};
+    int[] playerSpriteLeft = {1, 2};
+    int[] playerSpriteRight = {1, 2};
+
     private int temperature;
     private int shovelLevel;
     private int walkSpeed;
